4e72f99b3898d830c3196303df496f0fe2fe7d2d
Samlv9 <Samlv9@live.com>
2018-02-22 18:53:11 +0800

    添加单元测试

---
+++ test/EventDispatcher.test.js
@@ -0,0 +1,12 @@
+import { Event, EventDispatcher } from "..";
+
+
+describe("事件派发器(EventDispatcher)", () => {
+    /**
+     * 注册侦听器。
+     */
+    it("addEventListener", () => {
+        
+        
+    });
+});
